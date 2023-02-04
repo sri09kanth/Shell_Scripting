405ea1e93b74383e88f925dcf8e0283777f0b995
Srikanth Mergu <105923827+sri09kanth@users.noreply.github.com>
2023-02-04 11:13:28 +0530

    Update 6_command_subs.sh

--- 6_command_subs.sh
+++ 6_command_subs.sh
@@ -8,8 +8,6 @@ LOAD=`uptime | awk '{print $9}'`
 ROOTFREE=$(df -h | grep '/dev/sda1' | awk '{print $4}')
 
 
-echo "#######################################################"
-echo "Available free RAM is $FREERAM MB"
 
 
 
