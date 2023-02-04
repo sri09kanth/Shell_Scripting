6de8e2257eded007d0b5ccdf9145d1de2f2f4423
Srikanth Mergu <105923827+sri09kanth@users.noreply.github.com>
2023-02-04 11:12:41 +0530

    Update 6_command_subs.sh

--- 6_command_subs.sh
+++ 6_command_subs.sh
@@ -11,6 +11,6 @@ ROOTFREE=$(df -h | grep '/dev/sda1' | awk '{print $4}')
 echo "#######################################################"
 echo "Available free RAM is $FREERAM MB"
 echo "#######################################################"
-echo "Current Load Average $LOAD"
+
 
 
