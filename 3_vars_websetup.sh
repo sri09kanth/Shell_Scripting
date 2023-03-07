5c429a0f829f519d7d330539920a11e7f24f6733
Srikanth Mergu <105923827+sri09kanth@users.noreply.github.com>
2023-03-07 20:00:45 +0530

    Update 3_vars_websetup.sh

--- 3_vars_websetup.sh
+++ 3_vars_websetup.sh
@@ -10,7 +10,7 @@ TEMPDIR="/tmp/webfiles"
 # Installing Dependencies
 echo "########################################"
 echo "Installing packages."
-echo "########################################"
+echo "######################################
 sudo yum install $PACKAGE -y > /dev/nu
 echo
 
