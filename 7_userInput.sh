b03ed2c2a49abf382d2444fb6208fcb75a164486
Srikanth Mergu <105923827+sri09kanth@users.noreply.github.com>
2023-04-02 16:45:19 +0530

    Update 7_userInput.sh

--- 7_userInput.sh
+++ 7_userInput.sh
@@ -5,6 +5,6 @@ read SKILL
 
 echo "Your $SKILL skill is in high Demand in the IT Industry."
 read -p 'Usernme: ' S
-read -sp 'Passwor: ' p
+read -sp 'Passwr: ' p
 
 echo "Logn Succesfull: Welcome USER $USR,"
