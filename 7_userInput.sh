c3010afc93dd71241a082c68425a54c53349726b
Srikanth Mergu <105923827+sri09kanth@users.noreply.github.com>
2023-04-02 16:44:04 +0530

    Update 7_userInput.sh

--- 7_userInput.sh
+++ 7_userInput.sh
@@ -7,4 +7,4 @@ echo "Your $SKILL skill is in high Demand in the IT Industry."
 read -p 'Username: ' US
 read -sp 'Password: ' pa
 eho
-echo "Login Successfull: Welcome USER $USR,"
+echo "Logn Successfull: Welcome USER $USR,"
