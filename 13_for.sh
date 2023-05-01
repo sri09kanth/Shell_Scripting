5ed990934382e0f97e672968040fd4c92eef5780
Srikanth Mergu <105923827+sri09kanth@users.noreply.github.com>
2023-05-01 10:34:16 +0530

    Update 13_for.sh

--- 13_for.sh
+++ 13_for.sh
@@ -5,5 +5,5 @@ do
   echo "Looping....."
   sleep 
   echo "##################################################"
-  echo "Value of VAR1 is $V
+  echo "Value of VAR1 is $
   echo "############################################
